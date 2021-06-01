551f1072f35e2043a6997b0e4c0dc104ca40a8f2
Hariprasad <haridx.poojary@gmail.com>
2021-06-01 21:33:47 +0530

    Test - landing & posts page

--- the_site/blog/urls.py
+++ the_site/blog/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path("", ...),
+    path("", views.landing_page, name="landing_page"),
+    path("posts/", views.posts, name="posts"),
+    path("posts/<slug:post_slug>", views.post_details, name="post_details"),
 ]
--- the_site/blog/views.py
+++ the_site/blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def landing_page(request):
+    context = {}
+    return render(request, "blog/landing.html", context)
+
+
+def posts(request):
+    context = {}
+    return render(request, "blog/posts.html", context)
+
+
+def post_details(request, post_slug):
+    ...

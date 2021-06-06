0b7dbbe2d8e4b4c3d54f743206921700be677f4a
Hariprasad <haridx.poojary@gmail.com>
2021-06-06 13:41:36 +0530

    Navigation and Data rendering with dummy data

--- the_site/blog/views.py
+++ the_site/blog/views.py
@@ -1,16 +1,68 @@
 from django.shortcuts import render
 
 # Create your views here.
+from datetime import date
+
+# dummy post detail content
+all_posts = [
+    {
+        "slug": "the-mountains",
+        "image": "mountains.jpg",
+        "author": "Hariprasad",
+        "date": date(2021, 6, 5),
+        "title": "Why I Adore Mountains",
+        "excerpt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dicta illum repudiandae officia fuga repellat.",
+        "content": """
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dicta illum repudiandae officia fuga repellat.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dicta illum repudiandae officia fuga repellat.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dicta illum repudiandae officia fuga repellat.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dicta illum repudiandae officia fuga repellat.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dicta illum repudiandae officia fuga repellat.
+        """,
+    },
+    {
+        "slug": "the-coding",
+        "image": "coding.jpg",
+        "author": "Hariprasad",
+        "date": date(2021, 6, 5),
+        "title": "Why I Love Python",
+        "excerpt": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dicta illum repudiandae officia fuga repellat.",
+        "content": """
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dicta illum repudiandae officia fuga repellat.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dicta illum repudiandae officia fuga repellat.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dicta illum repudiandae officia fuga repellat.
+        
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dicta illum repudiandae officia fuga repellat.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dicta illum repudiandae officia fuga repellat.
+        """,
+    },
+]
+
+
 def index(request):
-    context = {}
+    sorted_post = sorted(all_posts, key=lambda post: post["date"])
+    recent_posts = sorted_post[-2:]
+    context = {
+        "posts": recent_posts,
+    }
     return render(request, "blog/index.html", context)
 
 
 def posts(request):
-    context = {}
+    context = {
+        "posts": all_posts,
+    }
     return render(request, "blog/all_posts.html", context)
 
 
 def post_details(request, post_slug):
-    context = {}
+    context = {
+        "the_post": None,
+    }
+    for post in all_posts:
+        if post["slug"] == post_slug:
+            context["the_post"] = post
+            break
+
     return render(request, "blog/post-details.html", context)

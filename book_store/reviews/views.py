bcb49f43d7837416a8ffc32e9cc2baaadfce08fc
Hariprasad <haridx.poojary@gmail.com>
2021-06-16 17:10:51 +0530

    basic form

--- book_store/reviews/views.py
+++ book_store/reviews/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
 def review(request):
-    return render(request, "reviews/review.html")
+
+    if request.method == "POST":
+        user_name = request.POST["username"]
+        context = {"username": user_name}
+        # return render(request, "reviews/thankyou.html", context)
+        return redirect("thank_you")
+
+    context = {}
+    return render(request, "reviews/review.html", context)
+
+
+def thank_you(request):
+    context = {}
+    return render(request, "reviews/thankyou.html", context)

f2ef198b858e9d015e9a65182bb97b0aadca50b0
Hariprasad <haridx.poojary@gmail.com>
2021-06-16 11:04:27 +0530

    new app reviews to practice forms

--- book_store/book_store/urls.py
+++ book_store/book_store/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", include("book_outlet.urls")),
+    path("reviews/", include("reviews.urls")),
     path("admin/", admin.site.urls),
 ]

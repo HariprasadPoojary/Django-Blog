b9fb22e2769328d7637308d31dc48cb1e924df4d
Hariprasad <haridx.poojary@gmail.com>
2021-06-24 09:34:26 +0530

    Use of CreateView

--- book_store/profiles/urls.py
+++ book_store/profiles/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.CreateProfileView.as_view(), name="create_profile"),
+    path("", views.UserProfileCreateView.as_view(), name="create_profile"),
 ]
--- book_store/profiles/views.py
+++ book_store/profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views import View
+from django.views.generic.edit import CreateView
 
 # Create your views here.
 from . import file_ops
@@ -7,19 +8,27 @@ from .forms import CreateProfileForm
 from .models import UserProfile
 
 
-class CreateProfileView(View):
-    def get(self, request):
-        form = CreateProfileForm()
-        context = {"form": form}
-        return render(request, "profiles/create_profile.html", context)
+class UserProfileCreateView(CreateView):
+    model = UserProfile
+    template_name = "profiles/create_profile.html"
+    fields = "__all__"
+    success_url = "/profile/"
 
-    def post(self, request):
-        submitted_form = CreateProfileForm(request.POST, request.FILES)
-        if submitted_form.is_valid():
-            user_image = UserProfile(user_image=request.FILES["user_image"])
-            user_image.save()
-            # file_ops.save_file(request.FILES["image"])
-            return redirect("create_profile")
-        else:
-            context = {"form": submitted_form}
-            return render(request, "profiles/create_profile.html", context)
+
+# * with normal view class
+# class CreateProfileView(View):
+#     def get(self, request):
+#         form = CreateProfileForm()
+#         context = {"form": form}
+#         return render(request, "profiles/create_profile.html", context)
+
+#     def post(self, request):
+#         submitted_form = CreateProfileForm(request.POST, request.FILES)
+#         if submitted_form.is_valid():
+#             user_image = UserProfile(user_image=request.FILES["user_image"])
+#             user_image.save()
+#             # file_ops.save_file(request.FILES["image"])
+#             return redirect("create_profile")
+#         else:
+#             context = {"form": submitted_form}
+#             return render(request, "profiles/create_profile.html", context)
